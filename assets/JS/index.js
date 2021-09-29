fdb6f750109f60df4b1790e7b1803f2a43bf9d4b
Dianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <67185107+Dianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-09-29 19:27:27 +0300

    third commit

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,6 @@
-const addTaskBtn = document.querySelector('.add-todo-wrapper');
+const addTaskBtn = document.querySelector('.add-task-btn');
 const deskTaskInput = document.querySelector('description-task');
+const todosWrapper = document.querySelector('.todos-wrapper');
 
 let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -9,26 +10,31 @@ let todoItemElem = [];
 function Task(description) {
     this.description = description;
     this.completed = this.completed;
-}
+};
 
 const createTemplate = (task, index) => {
-    return `     <div class="todo-item ${task.completed ? 'checked' : ''}">
-                    <div class="description">${task.description}</div>
-                    <div class="buttons">
-                        <input onclick="completeTasks(${index})" class="btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}>
-                        <button onclick="deleteTasks(${index})"class="btn-delete">Delete</button>
+    return  `
+            <div class='todo-item ${task.completed ? 'checked' : ''}'>
+                <div class='description'>${task.description}</div>
+                    <div class='buttons'>
+                        <input onclick='completeTasks(${index})' class='btn-complete' type='checkbox' ${task.completed ? 'checked' : ''}>
+                        <button onclick='deleteTasks(${index})' class='btn-delete'>Delete</button>
                     </div>
-                </div>`
-}
+                </div>
+            </div>
+            `
+};
+
 
 const filterTasks = () => {
     const activeTasks = tasks.lenght && tasks.filter(item => item.completed === false);
     const completedTasks = tasks.lenght && tasks.filter(item => item.completed === true);
     tasks = [...activeTasks, ...completedTasks]
-}
+};
+
 
 const fillHtmlList = () => {
-    todosWrapper.innerHTML = "";
+    todosWrapper.innerHTML = ' ';
     if (tasks.lenght > 0) {
         filterTasks();
         tasks.forEach((item, index) => {
@@ -36,36 +42,25 @@ const fillHtmlList = () => {
         });
         todoItemElem = document.querySelectorAll('.todo-item');
     }
-}
+};
 fillHtmlList();
 
 const updateLocal = () => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+};
 
 const completedTasks = index => {
     tasks[index].completed = !tasks[index].completed;
-    if (tasks[index].completed) {
-        todoItemElem[index].classList.add('checked');
-    }else{
-        todoItemElem[index].classList.remove('checked');
-    }
+    if (tasks[index].completed) {todoItemElem[index].classList.add('checked');} else {todoItemElem[index].classList.remove('checked');}
     updateLocal();
     fillHtmlList();
-}
+};
 
-addTaskBtn.addEventListener('click', () => {
+addTaskBtn.addEventListener = ('click', () => {
     tasks.push(new Task(deskTaskInput.value));
-    updateLocal();
-    fillHtmlList();
-    deskTaskInput.value = '';
-})
+    deskTaskInput.value = ' ';
+});
 
 const deleteTask = index => {
     todoItemElem[index].classList.add('delition');
-    setTimeout(() => {
-        tasks.splice(index, 1);
-        updateLocal();
-        fillHtmlList();
-    }, 500)
-}
+};
