af95ccd8509321a1c806a986d3fcc531fd56f757
Dianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <67185107+Dianaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2021-10-01 16:30:42 +0300

    started create new JS

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,66 +1,13 @@
-const addTaskBtn = document.querySelector('.add-task-btn');
-const deskTaskInput = document.querySelector('description-task');
-const todosWrapper = document.querySelector('.todos-wrapper');
+const addTasksBtn = document.querySelector('.addTasksBtn');
+const inputTask = document.querySelector('.description-task');
+const tasksList = document.querySelector('.tasksList');
+let task = document.createElement('li');
 
-let tasks;
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
-
-let todoItemElem = [];
-
-function Task(description) {
+function Task(description){
     this.description = description;
-    this.completed = this.completed;
 };
 
-const createTemplate = (task, index) => {
-    return  `
-            <div class='todo-item ${task.completed ? 'checked' : ''}'>
-                <div class='description'>${task.description}</div>
-                    <div class='buttons'>
-                        <input onclick='completeTasks(${index})' class='btn-complete' type='checkbox' ${task.completed ? 'checked' : ''}>
-                        <button onclick='deleteTasks(${index})' class='btn-delete'>Delete</button>
-                    </div>
-                </div>
-            </div>
-            `
-};
-
-
-const filterTasks = () => {
-    const activeTasks = tasks.lenght && tasks.filter(item => item.completed === false);
-    const completedTasks = tasks.lenght && tasks.filter(item => item.completed === true);
-    tasks = [...activeTasks, ...completedTasks]
-};
-
-
-const fillHtmlList = () => {
-    todosWrapper.innerHTML = ' ';
-    if (tasks.lenght > 0) {
-        filterTasks();
-        tasks.forEach((item, index) => {
-            todosWrapper.innerHTML += createTemplate(item, index);
-        });
-        todoItemElem = document.querySelectorAll('.todo-item');
-    }
-};
-fillHtmlList();
-
-const updateLocal = () => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-const completedTasks = index => {
-    tasks[index].completed = !tasks[index].completed;
-    if (tasks[index].completed) {todoItemElem[index].classList.add('checked');} else {todoItemElem[index].classList.remove('checked');}
-    updateLocal();
-    fillHtmlList();
-};
-
-addTaskBtn.addEventListener = ('click', () => {
-    tasks.push(new Task(deskTaskInput.value));
-    deskTaskInput.value = ' ';
+addTasksBtn.addEventListener = ('click', () => {
+    tasksList.appendChild(task);
+    task.push(new Task(inputTask.value));
 });
-
-const deleteTask = index => {
-    todoItemElem[index].classList.add('delition');
-};
